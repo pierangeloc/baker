07dea8ddbfaec31d87434f279922692ca810a0ed
Merlijn van Ittersum <merlijn.van.ittersum@ing.com>
2017-05-05 17:14:29 +0200

    added ui form generation experiment

--- http/src/main/scala/com/ing/baker/http/Routes.scala
+++ http/src/main/scala/com/ing/baker/http/Routes.scala
@@ -1,16 +1,24 @@
 package com.ing.baker.http
 
 import akka.http.scaladsl.server.Directives
+import com.ing.baker.compiler.CompiledRecipe
 import com.ing.baker.core.Baker
+import com.ing.baker.schema.SchemaType
 
 trait Routes extends Directives {
 
-  // val baker: Baker
+//  val baker: Baker
+
+  val resourceRoutes = pathPrefix("resources") { getFromResourceDirectory("") }
 
   val routes = pathPrefix("baker") {
-    path("hello") {
-      get {
+    path("event") {
+      post {
         complete("hello")
+      }
+    } ~ path("schemas") {
+      get {
+        complete("schemas")
       }
     }
   }
